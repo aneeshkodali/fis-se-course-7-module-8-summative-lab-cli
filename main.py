371be8924205d0f3bc2fb2aeeac9db61e182d615
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-02-19 17:26:57 -0600

    Create initial parser

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# imports
+import argparse
+
+# initialize parser
+parser = argparse.ArgumentParser(description="Project Management CLI Tool")
+
+# parse args from command line
+args = parser.parse_args()
+# check if func/subcommand passed
+if hasattr(args, 'func'):
+    args.func(args)
+else:
+    parser.print_help()
