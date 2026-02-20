a4b62f680ee70d50cd728947737f7f68a9fbcad9
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-02-19 23:41:15 -0600

    Add initial user class and parser logic

--- models/user.py
+++ models/user.py
@@ -1,7 +1,29 @@
 class User:
     
     # init
-    def __init__(self, name, email):
+    def __init__(self, id, name, email):
+        self.id = id
         self.name = name
         self.email = email
         self.projects = [] # initialize empty project list
+
+    # convert from class to dict (instance)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+        }
+    
+    # convert from dict to class (class)
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id=data['id'],
+            name=data['name'],
+            email=data['email']
+        )
+
+    # representation function
+    def __repr__(self):
+        return f"User(id={self.id}, name={self.name}, email={self.email})"
