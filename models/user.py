7dde8ce352dd49ce60c6554190ef3381eab48e0e
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-02-20 22:36:24 -0500

    Add getter and setter for user id

--- models/user.py
+++ models/user.py
@@ -26,3 +26,17 @@ class User:
     # representation function
     def __repr__(self):
         return f"User(id={self.id}, name={self.name}, email={self.email})"
+    
+    # id getter
+    @property
+    def id(self):
+        return self.id
+    
+    # id setter
+    @id.setter
+    def id(self, value):
+        # check for valid values
+        if not isinstance(value, int) or value < 1:
+            print(f"{value} is not a valid value for id")
+            raise ValueError
+        self._id = value
